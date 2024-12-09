6b91e2f6a17cca3f5d5958fc13f7e72abb98c5c3
ayush pansari <ayushpansari01092001@gmail.com>
2024-12-09 18:32:59 +0530

    done task two 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const addData = async () =>{
+    let randomNum = Math.ceil( Math.random()*100)
+    try {
+    const newData = await fetch(`https://swapi.dev/api/people/${randomNum}`);
+    let result = await newData.json();
+    let newObj = {name:result.name,id:randomNum};
+    setData([...data,newObj]);
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+ 
+ const deleteItem = async (index) =>{
+      let filterdData = [...data];
+      filterdData.splice(index,1);
+      setData(filterdData);
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div>
+        <button style={{height:"30px",width:"100px"}} onClick={addData}>Add record</button>
+        {
+          data.map((item,index)=>{
+            return(
+              <div key={index} style={{height:"50px",width:"300px",border:"1px solid #000",display:"flex",alignItems:"center",justifyContent:"space-around"}}>
+                <p>{item.name}</p>
+                <button onClick={() => deleteItem(index)}>delete</button>
+              </div>
+            )
+          })
+        }
+      
+      </div>
     </div>
   );
 }
